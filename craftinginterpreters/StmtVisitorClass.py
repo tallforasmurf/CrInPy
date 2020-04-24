fcd7db85b4d60997134ccfbc14d8622f2fed7bb3
Nat Picker <tallforasmurf@yahoo.com>
2020-04-24 14:15:00 -0700

    fix indention

--- craftinginterpreters/StmtVisitorClass.py
+++ craftinginterpreters/StmtVisitorClass.py
@@ -12,20 +12,20 @@ This work is licensed under a
 import Stmt
 class StmtVisitor:
     def visitBlockStmt(self, client:Stmt.Block):
-	raise NotImplementedError("No visitor defined for Stmt.Block")
+        raise NotImplementedError("No visitor defined for Stmt.Block")
     def visitClassStmt(self, client:Stmt.Class):
-	raise NotImplementedError("No visitor defined for Stmt.Class")
+        raise NotImplementedError("No visitor defined for Stmt.Class")
     def visitExpressionStmt(self, client:Stmt.Expression):
-	raise NotImplementedError("No visitor defined for Stmt.Expression")
+        raise NotImplementedError("No visitor defined for Stmt.Expression")
     def visitFunctionStmt(self, client:Stmt.Function):
-	raise NotImplementedError("No visitor defined for Stmt.Function")
+        raise NotImplementedError("No visitor defined for Stmt.Function")
     def visitIfStmt(self, client:Stmt.If):
-	raise NotImplementedError("No visitor defined for Stmt.If")
+        raise NotImplementedError("No visitor defined for Stmt.If")
     def visitPrintStmt(self, client:Stmt.Print):
-	raise NotImplementedError("No visitor defined for Stmt.Print")
+        raise NotImplementedError("No visitor defined for Stmt.Print")
     def visitReturnStmt(self, client:Stmt.Return):
-	raise NotImplementedError("No visitor defined for Stmt.Return")
+        raise NotImplementedError("No visitor defined for Stmt.Return")
     def visitVarStmt(self, client:Stmt.Var):
-	raise NotImplementedError("No visitor defined for Stmt.Var")
+        raise NotImplementedError("No visitor defined for Stmt.Var")
     def visitWhileStmt(self, client:Stmt.While):
-	raise NotImplementedError("No visitor defined for Stmt.While")
+        raise NotImplementedError("No visitor defined for Stmt.While")
