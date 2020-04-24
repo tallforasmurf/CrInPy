c990e0f9e195db70e5cd60ee7673f5b47fdfea34
Nat Picker <tallforasmurf@yahoo.com>
2020-04-24 12:23:10 -0700

    add return notation

--- craftinginterpreters/ExprVisitorClass.py
+++ craftinginterpreters/ExprVisitorClass.py
@@ -12,27 +12,27 @@ This work is licensed under a
 import Expr
 class ExprVisitor:
     # generic astVisitor
-    def visitAssign(self, client:Expr.Assign):
+    def visitAssign(self, client:Expr.Assign)->object:
         raise NotImplementedError("No visitor defined for Expr.Assign")
-    def visitBinary(self, client:Expr.Binary):
+    def visitBinary(self, client:Expr.Binary)->object:
         raise NotImplementedError("No visitor defined for Expr.Binary")
-    def visitCall(self, client:Expr.Call):
+    def visitCall(self, client:Expr.Call)->object:
         raise NotImplementedError("No visitor defined for Expr.Call")
-    def visitGet(self, client:Expr.Get):
+    def visitGet(self, client:Expr.Get)->object:
         raise NotImplementedError("No visitor defined for Expr.Get")
-    def visitGrouping(self, client:Expr.Grouping):
+    def visitGrouping(self, client:Expr.Grouping)->object:
         raise NotImplementedError("No visitor defined for Expr.Grouping")
-    def visitLiteral(self, client:Expr.Literal):
+    def visitLiteral(self, client:Expr.Literal)->object:
         raise NotImplementedError("No visitor defined for Expr.Literal")
-    def visitLogical(self, client:Expr.Logical):
+    def visitLogical(self, client:Expr.Logical)->object:
         raise NotImplementedError("No visitor defined for Expr.Logical")
-    def visitSet(self, client:Expr.Set):
+    def visitSet(self, client:Expr.Set)->object:
         raise NotImplementedError("No visitor defined for Expr.Set")
-    def visitSuper(self, client:Expr.Super):
+    def visitSuper(self, client:Expr.Super)->object:
         raise NotImplementedError("No visitor defined for Expr.Super")
-    def visitThis(self, client:Expr.This):
+    def visitThis(self, client:Expr.This)->object:
         raise NotImplementedError("No visitor defined for Expr.Unary")
-    def visitUnary(self, client:Expr.Unary):
+    def visitUnary(self, client:Expr.Unary)->object:
         raise NotImplementedError("No visitor defined for Expr.Unary")
-    def visitVariable(self, client:Expr.Variable):
+    def visitVariable(self, client:Expr.Variable)->object:
         raise NotImplementedError("No visitor defined for Expr.Variable")
